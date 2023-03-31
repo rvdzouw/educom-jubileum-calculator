ffe8d80167540accb377bd4958a519f50d6ac92f
Ruben van der Zouw <rubenvdzouw@gmail.com>
2023-03-31 13:26:27 +0200

    tekening calculator

--- calculator.php
+++ calculator.php
@@ -2,15 +2,15 @@
 
 
 
-$bday = new DateTime('11.4.1987'); // Your date of birth
-$today = new Datetime(date('m.d.y'));
-$diff = $today->diff($bday);
-printf(' Your age : %d years, %d month, %d days', $diff->y, $diff->m, $diff->d);
-printf("\n");
+$bday = strtotime('1987-4-11'); // Your date of birth
+$today = strtotime('now');
+// $diff = $today->diff($bday);
+// printf(' Your age : %d years, %d month, %d days', $diff->y, $diff->m, $diff->d);
+// printf("\n");
+$Now = new DateTime('now', new DateTimeZone('UTC'));
 
-$now = new DateTime();
-echo "4 digit of current year is: " . $now->format('Y');
-echo "<br />";
-echo "2 digit of current year is: " . $now->format('y');
+echo $bday . '<br>';
+echo $today . '<br>';
+echo $Now->format('Y-m-d');
 
 ?>
