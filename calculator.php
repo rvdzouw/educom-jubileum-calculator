b9494789cdcd2d45a4c850de6f5b7a278587bb45
Ruben van der Zouw <rubenvdzouw@gmail.com>
2023-04-03 09:58:10 +0200

    calculator

--- calculator.php
+++ calculator.php
@@ -1,16 +1,48 @@
 <?php
 
 
+$bday = new DateTime('11.4.1987'); // Your date of birth
+$today = new Datetime(date('m.d.y'));
+$diff = $today->diff($bday);
+printf(' Your age : %d', $diff->days);
+printf("\n");
 
-$bday = strtotime('1987-4-11'); // Your date of birth
-$today = strtotime('now');
-// $diff = $today->diff($bday);
-// printf(' Your age : %d years, %d month, %d days', $diff->y, $diff->m, $diff->d);
-// printf("\n");
-$Now = new DateTime('now', new DateTimeZone('UTC'));
+echo '<br>';
 
-echo $bday . '<br>';
-echo $today . '<br>';
-echo $Now->format('Y-m-d');
+echo date_diff(date_create('1987-04-11'), date_create('today'))->y;
+
+echo '<br>';
+
+function ageCalculator($dob){
+    if(!empty($dob)){
+        $birthdate = new DateTime($dob);
+        $today   = new DateTime('today');
+        $age = $birthdate->diff($today)->d;
+        return $age;
+    }else{
+        return 0;
+    }
+}
+$dob = '1987-04-11';
+echo ageCalculator($dob);
+
+echo '<br>';
+
+$c= date('d');
+$y= date('d',strtotime('1987-04-11'));
+echo $c-$y;
+
+echo '<br>';
+
+$dateOfBirth = "11-04-1987";
+$today = date("Y-m-d");
+$diff = date_diff(date_create($dateOfBirth), date_create($today));
+echo 'Age is '.$diff->format('%d');
+
+echo '<br>';
+
+$dob='1987-04-11';
+$year = (date('d') - date('d',strtotime($dob)));
+echo $year;
 
 ?>
